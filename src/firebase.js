005399c1c5b8396eb3be86ab170f55fd31fa4c10
unknown <haitammaharani@gmail.com>
2025-12-01 15:19:15 +0100

    add: add dependencies

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAH1XGn1nDTosTVas5ru6ZNLrgSTXiH2vI",
+  authDomain: "booknest-b8c54.firebaseapp.com",
+  projectId: "booknest-b8c54",
+  storageBucket: "booknest-b8c54.firebasestorage.app",
+  messagingSenderId: "618545903472",
+  appId: "1:618545903472:web:d9a907edf2f618a07101b5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
