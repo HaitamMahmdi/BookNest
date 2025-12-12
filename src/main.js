9768ebcccf3b9ccffb2ad88023a274c8cdb77140
unknown <haitammaharani@gmail.com>
2025-12-12 15:21:19 +0100

    feat: add more dependencies and fixs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 import { router } from "./router/router";
-createApp(App).use(router).mount("#app");
+import { fireApp } from "./firebase";
+const pinia = createPinia();
+createApp(App).use(router).use(fireApp).use(pinia).mount("#app");
