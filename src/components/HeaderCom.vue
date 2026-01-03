5d6ce9b36dd1b71a885a53df4c6e4a7aec2789c5
unknown <haitammaharani@gmail.com>
2026-01-03 06:13:08 +0100

    ui:add responsive design to element

--- src/components/HeaderCom.vue
+++ src/components/HeaderCom.vue
@@ -15,40 +15,98 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 const showDropdown = ref(false);
 const profile = ref(null);
-
-const handleClickOutside = (e) => {
-  if (!profile.value?.contains(e.target)) {
+const handleClick = (e) => {
+  console.log(e.target);
+  if (!showDropdown.value) {
+    showDropdown.value = true;
+    document.addEventListener("click", handleClick);
+    return;
+  }
+  console.log(e.target);
+  if (!profile.value?.contains(e?.target)) {
+    console.log(profile.value?.contains(e?.target));
     showDropdown.value = false;
+    document.removeEventListener("click", handleClick);
   }
 };
+/**    <ul
+      v-show="showDropdown"
+      class="absolute w-full sm:hidden bg-[#354445] z-50 text-text-main top-full"
+    >
+      <li class="flex items-center justify-center py-4">
+        <SearchbarCom></SearchbarCom>
+      </li>
+      <li
+        class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
+      >
+        <FontAwesomeIcon :icon="faHouse" />
+        <p>View Profile</p>
+      </li>
+      <li
+        class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
+      >
+        <FontAwesomeIcon :icon="faUser" />
+        <p>View Profile</p>
+      </li>
+      <li
+        class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
+      >
+        <FontAwesomeIcon :icon="faBookmark" />
+        <p>Your Shelfs</p>
+      </li>
+      <li
+        class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
+      >
+        <FontAwesomeIcon :icon="faTrophy" />
+        <p>Achievements</p>
+      </li>
+      <li class="w-full h-0.5 bg-[#dddd]"></li>
+      <li
+        class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
+      >
+        <FontAwesomeIcon :icon="faShieldHalved" />
+        <p>Premium</p>
+      </li>
 
-onMounted(() => {
-  document.addEventListener("click", handleClickOutside);
-});
-
-onUnmounted(() => {
-  document.removeEventListener("click", handleClickOutside);
-});
+      <li
+        class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
+      >
+        <FontAwesomeIcon :icon="faGears" />
+        <p>Settings</p>
+      </li>
+      <li class="w-full h-0.5 bg-[#dddd]"></li>
+      <li
+        class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
+      >
+        <FontAwesomeIcon :icon="faInfo" />
+        <p>Help</p>
+      </li>
+    </ul> */
 </script>
 <template>
-  <section
-    class="bg-[#375557] w-full h-14 flex flex-wrap justify-center items-center"
+  <header
+    class="bg-[#375557] relative sm:px-4 w-full h-14 flex flex-wrap justify-center items-center"
   >
-    <SearchbarCom :prop-class="'mr-4'"></SearchbarCom>
+    <div class="max-w-180 grow px-4 max-sm:hidden">
+      <SearchbarCom :prop-class="'mr-4'"></SearchbarCom>
+    </div>
     <button
       ref="profile"
-      @click="showDropdown = !showDropdown"
-      class="block h-full relative cursor-pointer"
+      class="block h-full max-sm:w-full pl-4 relative cursor-pointer"
     >
       <img
+        @click.self="handleClick($event)"
         class="w-11 aspect-square rounded-full"
         src="/assets/authors/author4.webp"
         alt="Profile"
       />
       <ul
         v-if="showDropdown"
-        class="absolute bg-[#354445] z-50 text-text-main top-full"
+        class="absolute bg-[#354445] z-50 text-text-main top-full right-0 max-sm:left-0 max-sm:w-full"
       >
+        <li class="flex items-center justify-center py-4 sm:hidden">
+          <SearchbarCom></SearchbarCom>
+        </li>
         <li
           class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
         >
@@ -96,5 +154,5 @@ onUnmounted(() => {
         </li>
       </ul>
     </button>
-  </section>
+  </header>
 </template>
