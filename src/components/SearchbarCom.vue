ea086b20612f954b4a99643648bee83adc2c1606
unknown <haitammaharani@gmail.com>
2026-01-03 06:12:21 +0100

    ui:add responsive design to element

--- src/components/SearchbarCom.vue
+++ src/components/SearchbarCom.vue
@@ -4,16 +4,15 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 const props = defineProps({ propClass: String });
 </script>
 <template>
-  <div :class="[props.propClass]" class="relative w-180 max-sm:px-2 h-10">
+  <div :class="[props.propClass]" class="relative w-full grow max-sm:px-2 h-10">
     <input
       type="text"
-      class="outline-0 peer block w-full h-full pl-12 bg-text-main rounded-full"
+      class="outline-0 peer block w-full h-full pl-12 bg-text-main text-text-secondary rounded-full"
     />
 
     <FontAwesomeIcon
       class="absolute right-4 peer-focus-visible:text-transparent text-[#333] top-3/6 transform -translate-y-3/6"
       :icon="faMagnifyingGlass"
     />
-    <div class="absolute w-full h-40 z-30"></div>
   </div>
 </template>
