e069da95ddd3a5526ea28eff555e5850cdb0629f
unknown <haitammaharani@gmail.com>
2026-01-03 05:03:36 +0100

    feat:update the create account action to create a Firestore user document on account creation

--- src/stores/userAuth.js
+++ src/stores/userAuth.js
@@ -6,7 +6,8 @@ import {
   signInWithEmailAndPassword,
   sendEmailVerification,
 } from "firebase/auth";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+import { getFirestore, collection, setDoc, doc } from "firebase/firestore";
 export const useUserAuth = defineStore(`userAuth`, {
   state: () => ({
     user: null,
@@ -32,9 +33,22 @@ export const useUserAuth = defineStore(`userAuth`, {
           url: "http://localhost:5173/",
           handleCodeInApp: false,
         };
+
+        const db = getFirestore();
+        const colRef = collection(db, "users");
+        await setDoc(doc(colRef, userCredential.user.uid), {
+          createdAt: new Date(),
+          favorites: [],
+          shelfs: [],
+          reading: [],
+        });
+
+        console.log("User document created in Firestore");
         await sendEmailVerification(userCredential.user, actionCodeSettings);
+        console.log("Verification email sent");
         return;
       } catch (error) {
+        console.error("Create account error:", error.message);
         throw error;
       }
     },
