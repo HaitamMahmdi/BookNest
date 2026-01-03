5e9a025d836d3e399da031f4963aae0c65a82efb
unknown <haitammaharani@gmail.com>
2026-01-03 06:16:56 +0100

    add the isfave prop to book info card component and more req to google books api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { computed, watch } from "vue";
-import { useUserAuth } from "./stores/userAuth";
 import HeaderCom from "./components/HeaderCom.vue";
+import { useUserAuth } from "./stores/userAuth";
 const userAuth = useUserAuth();
 userAuth.init();
 </script>
-
 <template>
-  <header-com></header-com>
+  <header-com v-if="userAuth.user"></header-com>
+
   <main class="bg-bg-main">
     <router-view></router-view>
   </main>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,53 +2,99 @@
 import { ref } from "vue";
 import BookCardCom from "../components/bookCard/BookCardCom.vue";
 import BookInfoCardCom from "../components/bookCard/BookInfoCardCom.vue";
-const books = ref(null);
+import { faDragon, faRobot, faGhost, faLandmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { useUserBooks } from "../stores/userBooks";
+ const userBooks = useUserBooks();
+const books = ref({});
+userBooks.loadUserBooks();
 (async () => {
   const resp = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&orderBy=newest&langRestrict=en&maxResults=40
-`
+    `https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&orderBy=newest&langRestrict=en&maxResults=40`
   );
   let data = await resp.json();
-  books.value = data.items;
-
+  books.value.fantasy = data.items;
   return;
 })();
 
+(async () => {
+  const resp = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=subject:science_fiction&orderBy=newest&langRestrict=en&maxResults=40
+`
+  );
+  let data = await resp.json();
+  books.value.science_fiction = data.items;
+  return;
+})();
+
+(async () => {
+  const resp = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=subject:mystery&orderBy=newest&langRestrict=en&maxResults=40`
+  );
+  let data = await resp.json();
+  books.value.mystery = data.items;
+  return;
+})();
+
+(async () => {
+  const resp = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=subject:history&orderBy=newest&langRestrict=en&maxResults=40`
+  );
+  let data = await resp.json();
+  books.value.history = data.items;
+  return;
+})();
+const fillteredBooks = ref(null);
 const bookDetail = ref(null);
 </script>
 <template>
-  <div class="grid lg:grid-cols-[320px_1fr]">
-    <section class="bg-bg-secondary min-h-screen max-lg:absolute"></section>
-    <section class="mt-8 pt-10 gap-x-50 flex flex-wrap">
-      <div class="flex flex-wrap max-w-240 gap-4 justify-center">
-        <BookCardCom
-          v-if="books"
-          v-for="book in books"
+  <div>
+    <section class="pt-10 min-h-screen ">
+      <ul class=" text-text-main flex flex-wrap h-fit gap-6 mb-8 w-full select-none justify-center  max-sm:text-sm">
+        <li @click="()=> fillteredBooks === 'fantasy' ? fillteredBooks = '' : fillteredBooks = 'fantasy' " :class="[fillteredBooks === 'fantasy' ? 'bg-white! text-text-secondary!' : '']" class=" flex h-fit items-center px-4 py-1 border rounded-full cursor-pointer      "><FontAwesomeIcon class="mr-2" :icon="faDragon" /> <p>fantasy</p></li>
+        <li @click="()=> fillteredBooks === 'science_fiction' ? fillteredBooks = '' : fillteredBooks = 'science_fiction'" :class="[fillteredBooks === 'science_fiction' ? 'bg-white! text-text-secondary!' : '']" class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer "><FontAwesomeIcon class="mr-2" :icon="faRobot" /> <p>science fiction</p></li>
+        <li @click="()=> fillteredBooks === 'mystery' ? fillteredBooks = '' : fillteredBooks = 'mystery'" :class="[fillteredBooks === 'mystery' ? 'bg-white! text-text-secondary!' : '']" class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer "><FontAwesomeIcon class="mr-2" :icon="faGhost" /> <p>mystery</p></li>
+        <li @click="()=> fillteredBooks === 'history' ? fillteredBooks = '' : fillteredBooks = 'history'" :class="[fillteredBooks === 'history' ? 'bg-white! text-text-secondary!' : '']" class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer "><FontAwesomeIcon class="mr-2" :icon="faLandmark" /> <p>history</p></li>
+      </ul> 
+      <div class="flex flex-wrap px-8 w-full gap-4 justify-center">
+        <BookCardCom 
+          v-if="books && (!fillteredBooks || fillteredBooks === 'fantasy')"
+          v-for="book in books.fantasy"
           :key="book.id"
           :book-obj="book"
           @showInfos="(bookObj) => (bookDetail = bookObj)"
         >
         </BookCardCom>
+         <BookCardCom
+          v-if="books && (!fillteredBooks || fillteredBooks === 'science_fiction')"
+          v-for="book in books.science_fiction"
+          :key="book.id"
+          :book-obj="book"
+          @showInfos="(bookObj) => (bookDetail = bookObj)"
+        >
+        </BookCardCom>
+         <BookCardCom
+          v-if="books && (!fillteredBooks || fillteredBooks === 'mystery')"
+          v-for="book in books.mystery"
+          :key="book.id"
+          :book-obj="book"
+          @showInfos="(bookObj) => (bookDetail = bookObj)"
+        >
+        </BookCardCom>
+          <BookCardCom
+            v-if="books && (!fillteredBooks || fillteredBooks === 'history')"
+            v-for="book in books.history"
+            :key="book.id"
+            :book-obj="book"
+            @showInfos="(bookObj) => (bookDetail = bookObj)"
+             </BookCardCom>
       </div>
-      <div v-if="false" class="w-80 h-200 pt-8 bg-bg-tertiary rounded-2xl">
-        <div class="w-40 h-62 mx-auto" v-if="bookDetail">
-          <img
-            class="w-full h-full"
-            :src="bookDetail.volumeInfo.imageLinks?.thumbnail"
-            :alt="bookDetail.volumeInfo.title"
-          />
-        </div>
-        <ul>
-          <li class="text-center font-semibold">
-            <p>{{ bookDetail.volumeInfo.title }}</p>
-            <p>{{ bookDetail.volumeInfo?.subtitle }}</p>
-          </li>
-        </ul>
-      </div>
+     
       <BookInfoCardCom
         @close="() => (bookDetail = null)"
-        v-if="bookDetail"
+        v-if="bookDetail && userBooks"
         :book-info="bookDetail"
+        :isFav="userBooks.favorites.some(favBook => favBook.id === bookDetail.id)"
       ></BookInfoCardCom>
     </section>
   </div>
