1c3d1a5b5c1be688d6d6faaf48c0200c4eb62d25
unknown <haitammaharani@gmail.com>
2026-04-27 16:57:48 +0200

    update bookcard in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@ import BookCardCom from "@/components/BookCard/BookCardCom.vue";
 import { faDragon, faRobot, faGhost, faLandmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { useUserBooks } from "../stores/userBooks";
- const userBooks = useUserBooks();
+const userBooks = useUserBooks();
 const books = ref({});
 (async () => {
   const resp = await fetch(
@@ -49,51 +49,48 @@ const bookDetail = ref(null);
   <div>
     <section class="pt-10 min-h-screen ">
       <ul class=" text-text-main flex flex-wrap h-fit gap-6 mb-8 w-full select-none justify-center  max-sm:text-sm">
-        <li @click="()=> fillteredBooks === 'fantasy' ? fillteredBooks = '' : fillteredBooks = 'fantasy' " :class="[fillteredBooks === 'fantasy' ? 'bg-white! text-text-secondary!' : '']" class=" flex h-fit items-center px-4 py-1 border rounded-full cursor-pointer      "><FontAwesomeIcon class="mr-2" :icon="faDragon" /> <p>fantasy</p></li>
-        <li @click="()=> fillteredBooks === 'science_fiction' ? fillteredBooks = '' : fillteredBooks = 'science_fiction'" :class="[fillteredBooks === 'science_fiction' ? 'bg-white! text-text-secondary!' : '']" class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer "><FontAwesomeIcon class="mr-2" :icon="faRobot" /> <p>science fiction</p></li>
-        <li @click="()=> fillteredBooks === 'mystery' ? fillteredBooks = '' : fillteredBooks = 'mystery'" :class="[fillteredBooks === 'mystery' ? 'bg-white! text-text-secondary!' : '']" class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer "><FontAwesomeIcon class="mr-2" :icon="faGhost" /> <p>mystery</p></li>
-        <li @click="()=> fillteredBooks === 'history' ? fillteredBooks = '' : fillteredBooks = 'history'" :class="[fillteredBooks === 'history' ? 'bg-white! text-text-secondary!' : '']" class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer "><FontAwesomeIcon class="mr-2" :icon="faLandmark" /> <p>history</p></li>
-      </ul> 
+        <li @click="() => fillteredBooks === 'fantasy' ? fillteredBooks = '' : fillteredBooks = 'fantasy'"
+          :class="[fillteredBooks === 'fantasy' ? 'bg-white! text-text-secondary!' : '']"
+          class=" flex h-fit items-center px-4 py-1 border rounded-full cursor-pointer      ">
+          <FontAwesomeIcon class="mr-2" :icon="faDragon" />
+          <p>fantasy</p>
+        </li>
+        <li
+          @click="() => fillteredBooks === 'science_fiction' ? fillteredBooks = '' : fillteredBooks = 'science_fiction'"
+          :class="[fillteredBooks === 'science_fiction' ? 'bg-white! text-text-secondary!' : '']"
+          class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer ">
+          <FontAwesomeIcon class="mr-2" :icon="faRobot" />
+          <p>science fiction</p>
+        </li>
+        <li @click="() => fillteredBooks === 'mystery' ? fillteredBooks = '' : fillteredBooks = 'mystery'"
+          :class="[fillteredBooks === 'mystery' ? 'bg-white! text-text-secondary!' : '']"
+          class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer ">
+          <FontAwesomeIcon class="mr-2" :icon="faGhost" />
+          <p>mystery</p>
+        </li>
+        <li @click="() => fillteredBooks === 'history' ? fillteredBooks = '' : fillteredBooks = 'history'"
+          :class="[fillteredBooks === 'history' ? 'bg-white! text-text-secondary!' : '']"
+          class=" flex  h-fit items-center px-4 py-1 border rounded-full cursor-pointer ">
+          <FontAwesomeIcon class="mr-2" :icon="faLandmark" />
+          <p>history</p>
+        </li>
+      </ul>
       <div class="flex flex-wrap px-8 w-full gap-4 justify-center">
-        <BookCardCom 
-          v-if="books && (!fillteredBooks || fillteredBooks === 'fantasy')"
-          v-for="book in books.fantasy"
-          :key="book.id"
-          :book="book"
-          @showInfos="(bookObj) => (bookDetail = bookObj)"
-        >
+        <BookCardCom v-if="books && (!fillteredBooks || fillteredBooks === 'fantasy')" v-for="book in books.fantasy"
+          :key="book.id" :book="book">
         </BookCardCom>
-         <BookCardCom
-          v-if="books && (!fillteredBooks || fillteredBooks === 'science_fiction')"
-          v-for="book in books.science_fiction"
-          :key="book.id"
-          :book="book"
-          @showInfos="(bookObj) => (bookDetail = bookObj)"
-        >
+        <BookCardCom v-if="books && (!fillteredBooks || fillteredBooks === 'science_fiction')"
+          v-for="book in books.science_fiction" :key="book.id" :book="book"
+          @showInfos="(bookObj) => (bookDetail = bookObj)">
         </BookCardCom>
-         <BookCardCom
-          v-if="books && (!fillteredBooks || fillteredBooks === 'mystery')"
-          v-for="book in books.mystery"
-          :key="book.id"
-          :book="book"
-          @showInfos="(bookObj) => (bookDetail = bookObj)"
-        >
+        <BookCardCom v-if="books && (!fillteredBooks || fillteredBooks === 'mystery')" v-for="book in books.mystery"
+          :key="book.id" :book="book">
         </BookCardCom>
-          <BookCardCom
-            v-if="books && (!fillteredBooks || fillteredBooks === 'history')"
-            v-for="book in books.history"
-            :key="book.id"
-            :book="book"
-            @showInfos="(bookObj) => (bookDetail = bookObj)"
-             </BookCardCom>
+        <BookCardCom v-if="books && (!fillteredBooks || fillteredBooks === 'history')" v-for="book in books.history"
+          :key="book.id" :book="book" </BookCardCom>
       </div>
-     
-      <BookInfoCardCom
-        @close="() => (bookDetail = null)"
-        v-if="bookDetail && userBooks"
-        :book-info="bookDetail"
-        :isFav="userBooks.favorites.some(favBook => favBook.id === bookDetail.id)"
-      ></BookInfoCardCom>
+
+
     </section>
   </div>
 </template>
