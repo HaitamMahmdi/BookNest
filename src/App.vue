abe596189aef52d2b2be4d667e17c1d1422635fa
unknown <haitammaharani@gmail.com>
2026-05-04 19:33:37 +0200

    delete globle style and header is fixed

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ onUnmounted(() => {
 <template>
   <header-com v-if="userAuth"></header-com>
 
-  <main class="bg-bg-main min-h-screen py-20">
+  <main class="bg-bg-main min-h-screen">
     <router-view :key="$route.fullPath"></router-view>
   </main>
   <FooterCom></FooterCom>
--- src/components/HeaderCom.vue
+++ src/components/HeaderCom.vue
@@ -29,71 +29,57 @@ const handleClick = (e) => {
 };
 </script>
 <template>
-  <header
-    class="bg-[#375557] relative sm:px-4 w-full h-14 flex flex-wrap justify-center items-center"
-  >
+  <header class="bg-[#375557] fixed z-40 sm:px-4 w-full h-14 flex flex-wrap justify-center items-center">
     <div class="max-w-180 grow px-4 max-sm:hidden">
       <SearchbarCom :prop-class="'mr-4'"></SearchbarCom>
     </div>
-    <button
-      ref="profile"
-      class="block h-full max-sm:w-full pl-4 relative cursor-pointer"
-    >
-      <img
-        @click.self="handleClick($event)"
-        class="w-11 aspect-square rounded-full"
-        src="/assets/authors/author4.webp"
-        alt="Profile"
-      />
-      <ul
-        v-if="showDropdown"
-        class="absolute bg-[#354445] z-50 text-text-main top-full right-0 max-sm:left-0 max-sm:w-full"
-      >
+    <button ref="profile" class="block h-full max-sm:w-full pl-4 relative cursor-pointer">
+      <img @click.self="handleClick($event)" class="w-11 aspect-square rounded-full" src="/assets/authors/author4.webp"
+        alt="Profile" />
+      <ul v-if="showDropdown"
+        class="absolute bg-[#354445] z-50 text-text-main top-full right-0 max-sm:left-0 max-sm:w-full">
         <li class="flex items-center justify-center py-4 sm:hidden">
           <SearchbarCom></SearchbarCom>
         </li>
         <li
-          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
-        >
-          <FontAwesomeIcon :icon="faHouse" />
-          <p>View Profile</p>
+          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2">
+          <RouterLink :to="{ name: 'Home' }" class="flex items-center gap-x-2">
+            <FontAwesomeIcon :icon="faHouse" />
+            <p>Home</p>
+          </RouterLink>
         </li>
         <li
-          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
-        >
-          <FontAwesomeIcon :icon="faUser" />
-          <p>View Profile</p>
+          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2">
+          <RouterLink :to="{ name: 'Profile' }" class="flex items-center gap-x-2">
+            <FontAwesomeIcon :icon="faUser" />
+            <p>View Profile</p>
+          </RouterLink>
         </li>
         <li
-          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
-        >
+          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2">
           <FontAwesomeIcon :icon="faBookmark" />
           <p>Your Shelfs</p>
         </li>
         <li
-          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
-        >
+          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2">
           <FontAwesomeIcon :icon="faTrophy" />
           <p>Achievements</p>
         </li>
         <li class="w-full h-0.5 bg-[#dddd]"></li>
         <li
-          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
-        >
+          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2">
           <FontAwesomeIcon :icon="faShieldHalved" />
           <p>Premium</p>
         </li>
 
         <li
-          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
-        >
+          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2">
           <FontAwesomeIcon :icon="faGears" />
           <p>Settings</p>
         </li>
         <li class="w-full h-0.5 bg-[#dddd]"></li>
         <li
-          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2"
-        >
+          class="flex items-center w-75 hover:pl-8 hover:bg-bg-secondary transform duration-100 pl-4 pr-10 py-4 font-semibold gap-x-2">
           <FontAwesomeIcon :icon="faInfo" />
           <p>Help</p>
         </li>
