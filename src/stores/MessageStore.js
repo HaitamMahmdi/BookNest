70690fe568d428d488aa9e8a5cf878d00f1b71f0
HaitamMahmdi <haitammaharani@gmail.com>
2026-05-28 16:34:12 +0100

    add Pinia store for messages component

---
+++ src/stores/MessageStore.js
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+
+export const useMessageStore = defineStore("messageStore", {
+  store: () => ({
+    messageText: "",
+    messageType: "",
+  }),
+  actions: {
+    updateMessage(text, type) {
+      this.messageText = text;
+      this.messageType = type;
+    },
+    dismissMessage() {
+      this.messageText = "";
+      this.messageType = "";
+    },
+  },
+});
